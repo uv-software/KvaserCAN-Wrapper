79c35828daf3e61e811f37f7c6789900b6f37c27
Uwe Vogt <uwe.vogt@uv-software.de>
2024-03-12 20:05:50 +0100

    Update build jobs for GitHub actions
    Relates to [CANAPI-66]

--- x64_build.bat
+++ x64_build.bat
@@ -1,29 +1,49 @@
 @echo off
 
-pushd
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" x64
-popd
+rem parse arguments: [[NOVARS] NOTRIAL]
+if "%1" == "NOVARS" (
+   set VCVARS="False"
+) else (
+   set VCVARS="True"
+)
+SHIFT
+if "%1" == "NOTRIAL" (
+  set TRIAL="False"
+) else (
+  set TRIAL="True"
+)
 
+rem set MSBuild environment variables
+if %VCVARS% == "True" (
+   pushd
+   call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" x64
+   popd
+)
 pushd
+
+rem generate a pseudo build number
 call build_no.bat
-rem type .\Sources\build_no.h
-rem pause
 
-call msbuild.exe .\Trial\kvl_test.vcxproj /t:Clean;Build /p:"Configuration=Debug";"Platform=x64"
-if errorlevel 1 goto end
-
+rem build the trial program
+if %TRIAL% == "True" ( 
+  call msbuild.exe .\Trial\kvl_test.vcxproj /t:Clean;Build /p:"Configuration=Debug";"Platform=x64"
+  if errorlevel 1 goto end
+)
+rem build the CAN API V3 C library (dynamic and static)
 call msbuild.exe .\Libraries\CANAPI\uvcankvl.vcxproj /t:Clean;Build /p:"Configuration=Release_dll";"Platform=x64"
 if errorlevel 1 goto end
 
 call msbuild.exe .\Libraries\CANAPI\uvcankvl.vcxproj /t:Clean;Build /p:"Configuration=Debug_lib";"Platform=x64"
 if errorlevel 1 goto end
 
+rem build the CAN API V3 C++ library (dynamic and static)
 call msbuild.exe .\Libraries\KvaserCAN\KvaserCAN.vcxproj /t:Clean;Build /p:"Configuration=Release_dll";"Platform=x64"
 if errorlevel 1 goto end
 
 call msbuild.exe .\Libraries\KvaserCAN\KvaserCAN.vcxproj /t:Clean;Build /p:"Configuration=Debug_lib";"Platform=x64"
 if errorlevel 1 goto end
 
+rem copy the arifacts into the Binaries folder
 echo Copying artifacts...
 set BIN=".\Binaries"
 if not exist %BIN% mkdir %BIN%
@@ -53,12 +73,14 @@ copy /Y .\Sources\CANAPI\CANBTR_Defaults.h %INC%
 copy /Y .\Sources\CANAPI\can_api.h %INC%
 copy /Y .\Sources\CANAPI\can_btr.h %INC%
 
+rem build the utilities 'can_mone' and 'can_test'
 call msbuild.exe .\Utilities\can_moni\can_moni.vcxproj /t:Clean;Build /p:"Configuration=Release";"Platform=x64"
 if errorlevel 1 goto end
 
 call msbuild.exe .\Utilities\can_test\can_test.vcxproj /t:Clean;Build /p:"Configuration=Release";"Platform=x64"
 if errorlevel 1 goto end
 
+rem copy the utilities into the Binaries folder
 echo Copying utilities...
 set BIN=".\Binaries"
 if not exist %BIN% mkdir %BIN%
@@ -67,6 +89,9 @@ if not exist %BIN% mkdir %BIN%
 copy /Y .\Utilities\can_moni\x64\Release\can_moni.exe %BIN%
 copy /Y .\Utilities\can_test\x64\Release\can_test.exe %BIN%
 
+rem end of the job
 :end
 popd
-pause
+if %VCVARS% == "True" (
+   pause
+)
