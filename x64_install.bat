56e8af09fe8b0f63226b80274845d9de6e9e34af
Uwe Vogt <uwe.vogt@uv-software.de>
2024-04-07 18:11:30 +0200

    Update installation scripts
    Relates to [CANAPI-80]

--- x64_install.bat
+++ x64_install.bat
@@ -1,9 +1,10 @@
+rem Requires administrator rights!
 @echo off
 set PWD="%~dp0"
 pushd
 cd /D %PWD%
-copy /Y .\Libraries\CANAPI\x64\Release_dll\u3cankvl.dll C:\Windows\System32
-copy /Y .\Libraries\KvaserCAN\x64\Release_dll\uvKvaserCAN.dll C:\Windows\System32
+copy /Y .\Binaries\x64\u3cankvl.dll C:\Windows\System32
+copy /Y .\Binaries\x64\uvKvaserCAN.dll C:\Windows\System32
 popd
 dir C:\Windows\System32\u*can*.dll
 pause
--- x86_install.bat
+++ x86_install.bat
@@ -1,9 +1,10 @@
+rem Requires administrator rights!
 @echo off
 set PWD="%~dp0"
 pushd
 cd /D %PWD%
-copy /Y .\Libraries\CANAPI\Release_dll\u3cankvl.dll C:\Windows\SysWOW64
-copy /Y .\Libraries\KvaserCAN\Release_dll\uvKvaserCAN.dll C:\Windows\SysWOW64
+copy /Y .\Binaries\x86\u3cankvl.dll C:\Windows\SysWOW64
+copy /Y .\Binaries\x86\uvKvaserCAN.dll C:\Windows\SysWOW64
 popd
 dir C:\Windows\SysWOW64\u*can*.dll
 pause
