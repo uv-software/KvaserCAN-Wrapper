c96aa814b4f63fef80d36fb13e7b947d3feb276f
Uwe Vogt <uwe.vogt@uv-software.de>
2024-06-08 20:11:30 +0200

    Integrate CAN API V3 utilities (rev. 1323)
    Relates to [CANAPI-95]

--- Utilities/can_moni/Driver.h
+++ Utilities/can_moni/Driver.h
@@ -27,8 +27,10 @@
 #else
 #define CAN_FD_SUPPORTED   1  // don't touch that dial
 #endif
-#define MONITOR_INTEFACE  "Kvaser CAN Interfaces"
-#define MONITOR_COPYRIGHT "2007,2012-2024 by Uwe Vogt, UV Software, Berlin"
+#define MONITOR_INTERFACE  "Kvaser CAN Interfaces"
+#define MONITOR_COPYRIGHT  "2007,2012-2024 by Uwe Vogt, UV Software, Berlin"
+#define MONITOR_PLATFORM   "Windows"
+#define MONITOR_ALIASNAME  "KVL:"
 
 #if (OPTION_KVASER_BIT_TIMING == 1)
 #define BITRATE_1M(x)    KVASER_CAN_BR_1M(x)
@@ -41,16 +43,16 @@
 #define BITRATE_20K(x)   KVASER_CAN_BR_20K(x)
 #define BITRATE_10K(x)   KVASER_CAN_BR_10K(x)
 #else
-#define BITRATE_1M(x)    DEFAULT_CAN_BR_1M(x)  
+#define BITRATE_1M(x)    DEFAULT_CAN_BR_1M(x)
 #define BITRATE_800K(x)  DEFAULT_CAN_BR_800K(x)
 #define BITRATE_500K(x)  DEFAULT_CAN_BR_500K(x)
 #define BITRATE_250K(x)  DEFAULT_CAN_BR_250K(x)
 #define BITRATE_125K(x)  DEFAULT_CAN_BR_125K(x)
 #define BITRATE_100K(x)  DEFAULT_CAN_BR_100K(x)
-#define BITRATE_50K(x)   DEFAULT_CAN_BR_50K(x) 
-#define BITRATE_20K(x)   DEFAULT_CAN_BR_20K(x) 
-#define BITRATE_10K(x)   DEFAULT_CAN_BR_10K(x) 
-#define BITRATE_5K(x)    DEFAULT_CAN_BR_5K(x)  
+#define BITRATE_50K(x)   DEFAULT_CAN_BR_50K(x)
+#define BITRATE_20K(x)   DEFAULT_CAN_BR_20K(x)
+#define BITRATE_10K(x)   DEFAULT_CAN_BR_10K(x)
+#define BITRATE_5K(x)    DEFAULT_CAN_BR_5K(x)
 #endif
 #if (CAN_FD_SUPPORTED != 0)
 #define BITRATE_FD_1M(x)      KVASER_CAN_FD_BR_1M(x)
